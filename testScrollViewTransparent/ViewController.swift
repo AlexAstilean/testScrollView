f7032508046ceffdcd9eea46a43d0ac130152efb
akaur <32915938+akaur@users.noreply.github.com>
2018-07-31 16:11:37 -0700

    test
    test scrollview

--- testScrollViewTransparent/ViewController.swift
+++ testScrollViewTransparent/ViewController.swift
@@ -14,6 +14,21 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //Mark: - hide the navigation bar on the this view controller and show on other view controllers. make status bar translucent
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.navigationController?.setNavigationBarHidden(true, animated: animated)
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: .default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        self.navigationController?.navigationBar.isTranslucent = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.navigationController?.setNavigationBarHidden(false, animated: animated)
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
